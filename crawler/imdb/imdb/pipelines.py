d823bd44fe27461246ddfbd261ad4d1b76e032d6
lYesterdaYl <bgmds@126.com>
2019-02-01 22:34:40 -0800

    feat
    updated to use SQL to store crawled data.

--- crawler/imdb/imdb/pipelines.py
+++ crawler/imdb/imdb/pipelines.py
@@ -1,11 +1,49 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, BIGINT
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+Base = declarative_base()
+
+
+class IMDB_Movie_Info(Base):
+    __tablename__ = 'imdb_movie_info'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    certificate = Column(String(10))
+    run_time = Column(String(10))
+    genre = Column(String(80), nullable=False)
+    summary = Column(String(255), nullable=False)
+    rating = Column(Float)
+    rating_count = Column(Integer)
+    gross = Column(BIGINT)
+    actor = Column(String(255), nullable=False)
+    serial = Column(String(20))
 
 
 class ImdbPipeline(object):
+    def __init__(self):
+        DIALCT = "mysql"
+        DRIVER = "pymysql"
+        USERNAME = "root"
+        PASSWORD = ""
+        HOST = "127.0.0.1"
+        PORT = "3306"
+        DATABASE = "item_catalog"
+        DB_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8" \
+            .format(DIALCT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+        self.engine = create_engine(DB_URI)
+        Base.metadata.create_all(self.engine)
+        self.sess = sessionmaker(bind=self.engine)
+        self.session = self.sess()
+
     def process_item(self, item, spider):
+        self.session.add(IMDB_Movie_Info(**item))
+        self.session.commit()
+
         return item
+
+    def close_spider(self, spider):
+        self.session.close()
