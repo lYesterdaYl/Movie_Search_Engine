c7511e973cceaff52600ef7d9f4016ed12fa6a45
lYesterdaYl <bgmds@126.com>
2019-02-10 12:06:24 -0800

    feat
    updated analyzer class.

---
+++ analysis/analyzer.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_structure import IMDB_Movie_Summary, IMDB_Movie_Info
+
+Base = declarative_base()
+
+
+class Analyzer:
+
+    def __init__(self):
+        DIALCT = "mysql"
+        DRIVER = "pymysql"
+        USERNAME = "root"
+        PASSWORD = ""
+        HOST = "127.0.0.1"
+        PORT = "3306"
+        DATABASE = "imdb"
+        DB_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8" \
+            .format(DIALCT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+        engine = create_engine(DB_URI)
+        Base.metadata.create_all(engine)
+        sess = sessionmaker(bind=engine)
+        self.session = sess()
+        self.year = [1950, 2019]
+
+    def get_summary_data(self):
+        summary_data = self.session.query(IMDB_Movie_Summary.summary).join(IMDB_Movie_Info, IMDB_Movie_Summary.movie_id==IMDB_Movie_Info.id).filter(IMDB_Movie_Info.year>self.year[0], IMDB_Movie_Info.year<self.year[1])
+
+        return summary_data
+
+    def analyze(self):
+        pass
