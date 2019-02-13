fafb8edfbcb5a2745ca34962c7c81820d5e06a30
lYesterdaYl <bgmds@126.com>
2019-02-13 04:45:03 -0800

    feat
    1. updated analysis function to put movie title, year, genre and actor into consideration.
    2. updated update database function to commit the changes every loop cycle to prevent suddent stop causes losing all progress.
    3. added a progress info for both pre-processing and update database.

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -8,10 +8,9 @@ from nltk.stem import PorterStemmer
 import json
 from collections import Counter
 from operator import itemgetter
-
+import timeit
 
 Base = declarative_base()
-
 
 class Analyzer:
 
@@ -31,9 +30,10 @@ class Analyzer:
         self.session = sess()
         self.year = [1950, 2019]
 
-    def get_summary_data(self):
+    def get_movie_data(self):
         summary_data = self.session\
-            .query(IMDB_Movie_Summary.summary, IMDB_Movie_Info.id)\
+            .query(IMDB_Movie_Summary.summary, IMDB_Movie_Info.id, IMDB_Movie_Info.title
+                   , IMDB_Movie_Info.actor, IMDB_Movie_Info.genre, IMDB_Movie_Info.year)\
             .join(IMDB_Movie_Info, IMDB_Movie_Summary.movie_id == IMDB_Movie_Info.id)\
             .filter(IMDB_Movie_Info.year >= self.year[0], IMDB_Movie_Info.year <= self.year[1])
 
@@ -64,41 +64,74 @@ class Analyzer:
         return result
 
     def update_database(self, data: {}):
+        start = timeit.default_timer()
+        total = len(data)
+        n = 1
         for word, tf in data.items():
-
-            word_data = self.session.query(IMDB_Index_Data).filter_by(word=word).first()
+            stop = timeit.default_timer()
+            print("Update-Database-Progress: ", round(n/total*100, 3), "%", "   Used Time: ", round(stop - start, 3), "   Estimate Remain Time: ", round(100/round(n/total*100, 3)*round(stop-start, 3) - round(stop - start, 3), 3), " seconds")
+            word_data = self.session.query(IMDB_Index_Data.document_id).filter_by(word=word).first()
             if word_data is None:
                 tf = dict(sorted(tf.items(), key=itemgetter(1), reverse=True))
                 self.session.add(IMDB_Index_Data(word=word, document_id=json.dumps(tf)))
             else:
-                print("Word data = ", str(word_data))
-                print(word)
-                print(tf)
-                document_id = json.loads(word_data.document_id)
-                print(document_id)
                 word_data.document_id = dict(Counter(json.loads(word_data.document_id)) + Counter(tf))
                 word_data.document_id = json.dumps(dict(sorted(word_data.document_id.items(), key=itemgetter(1), reverse=True)))
                 self.session.add(word_data)
-        self.session.commit()
+            n += 1
+            self.session.commit()
 
 
     def analyze(self):
         result = {}
-        datas = self.get_summary_data()
+        datas = self.get_movie_data()
+        start = timeit.default_timer()
+        total = datas.count()
+
+        n = 1
         for data in datas:
+            stop = timeit.default_timer()
+            print("Pre-Processing-Progress: ", round(n / total * 100, 3), "%", "   Used Time: ", round(stop - start, 3),
+                  "   Estimate Remain Time: ",
+                  round(100 / round(n / total * 100, 3) * round(stop - start, 3) - round(stop - start, 3), 3),
+                  " seconds")
             sw = self.stop_word(data.summary)
             st = self.stemming(sw)
-            finished_dict = self.process_with_tf(st)
+            summary_dict = self.process_with_tf(st)
+
+            sw = self.stop_word(data.title)
+            st = self.stemming(sw)
+            title_dict = self.process_with_tf(st)
+
+            sw = self.stop_word(data.genre)
+            st = self.stemming(sw)
+            genre_dict = self.process_with_tf(st)
+
+            sw = self.stop_word(data.year)
+            st = self.stemming(sw)
+            year_dict = self.process_with_tf(st)
+
+            sw = self.stop_word(data.actor)
+            st = self.stemming(sw)
+            actor_dict = self.process_with_tf(st)
+
+            movie_info_dict = dict(Counter(title_dict) + Counter(genre_dict) + Counter(year_dict) + Counter(actor_dict))
+            for item in movie_info_dict:
+                movie_info_dict[item] += 100
+
+
+            finished_dict = dict(Counter(summary_dict) + Counter(movie_info_dict))
             for key, value in finished_dict.items():
+
                 if key not in result:
                     result[key] = {}
                 if data.id not in result[key]:
                     result[key][data.id] = value
-        print(result)
-        self.update_database(result)
+            n += 1
+        # self.update_database(result)
 
 if __name__ == '__main__':
     analyzer = Analyzer()
-    analyzer.year = [1950, 1950]
+    analyzer.year = [1951, 1951]
     analyzer.analyze()
 
