01a0f416f941b3c18e2810328a9555da7354c0bd
lYesterdaYl <bgmds@126.com>
2019-03-02 16:01:48 -0800

    feat
    updated to use the new index data.

--- Movie_App/movie_app/__init__.py
+++ Movie_App/movie_app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, redirect, \
     url_for, flash, jsonify, make_response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_structure import IMDB_Index_Data, IMDB_Movie_Info, Base
+from database_structure import IMDB_Index_Data, IMDB_Movie_Info, Base, IMDB_Info_Index_Data, IMDB_Summary_Index_Data
 import json
-from analysis.analyzer_2 import Analyzer
+from analysis.analyzer_3 import Analyzer
 from collections import Counter
 from operator import itemgetter
 from collections import OrderedDict
@@ -74,42 +74,51 @@ def index():
         # return "OK"
         return render_template('index.html')
 
-# @app.route('/', methods=['GET', 'POST'])
-# @app.route('/index', methods=['GET', 'POST'])
-# def query():
-#     if request.method == 'POST':
-#         analyzer = Analyzer()
-#         search_query = request.form['query']
-#         stop_word = analyzer.stop_word(search_query)
-#         stem = analyzer.stemming(stop_word)
-#
-#         result = Counter({})
-#         for word in set(stem.split(" ")):
-#             word = session.query(IMDB_Index_Data).filter_by(word=word).first()
-#             result += result + Counter(json.loads(word.document_id))
-#
-#         result = dict(sorted(result.items(), key=itemgetter(1), reverse=True))
-#         s = ""
-#
-#         n = 0
-#         for movie_id, tf in result.items():
-#             if n < 10:
-#                 movie_info = session.query(IMDB_Movie_Info.title, IMDB_Movie_Info.year).filter_by(id=movie_id).first()
-#                 s += "Title: " + str(movie_info.title) + " "
-#                 s += "Year: " + str(movie_info.year) + " "
-#                 # s += "Certificate: " + str(movie_info.certificate) + " "
-#                 # s += "Run Time: " + str(movie_info.run_time) + " "
-#                 # s += "Genre: " + str(movie_info.genre) + " "
-#                 # s += "Rating: " + str(movie_info.rating) + " "
-#                 # s += "Rating Count: " + str(movie_info.rating_count) + " "
-#                 # s += "gross: " + str(movie_info.gross) + " "
-#                 # s += "Actor: " + str(movie_info.actor) + " "
-#                 s += "\n\n"
-#                 n += 1
-#             else:
-#                 break
-#
-#         return s
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        analyzer = Analyzer()
+        search_query = request.form['query']
+        stop_word = analyzer.stop_word(search_query)
+        stem = analyzer.stemming(stop_word)
+
+        info_result = Counter({})
+        summary_result = Counter({})
+        for word in set(stem.split(" ")):
+            info_word = session.query(IMDB_Info_Index_Data).filter_by(word=word).first()
+            summary_word = session.query(IMDB_Summary_Index_Data).filter_by(word=word).first()
+            if info_word is not None:
+                info_result += info_result + Counter(json.loads(info_word.document_id))
+            if summary_word is not None:
+                summary_result += summary_result + Counter(json.loads(summary_word.document_id))
+
+        result = info_result + summary_result
+        result = OrderedDict(sorted(result.items(), key=lambda d: d[1], reverse=True))
+        s = ""
+
+        n = 0
+        for movie_id, tf in result.items():
+            if n < 10:
+                movie_info = session.query(IMDB_Movie_Info.title, IMDB_Movie_Info.year).filter_by(id=movie_id).first()
+                s += "Title: " + str(movie_info.title) + " "
+                s += "Year: " + str(movie_info.year) + " "
+                # s += "Certificate: " + str(movie_info.certificate) + " "
+                # s += "Run Time: " + str(movie_info.run_time) + " "
+                # s += "Genre: " + str(movie_info.genre) + " "
+                # s += "Rating: " + str(movie_info.rating) + " "
+                # s += "Rating Count: " + str(movie_info.rating_count) + " "
+                # s += "gross: " + str(movie_info.gross) + " "
+                # s += "Actor: " + str(movie_info.actor) + " "
+                s += "<br><br>"
+                n += 1
+            else:
+                break
+
+        s += str(result)
+        return s
+    else:
+        return render_template('search.html')
+
 
 if __name__ == '__main__':
     app.secret_key = "secret_key"
