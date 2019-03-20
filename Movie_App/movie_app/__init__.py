b27625130c0e8ace55ee0c249e88a424f13080e8
lYesterdaYl <bgmds@126.com>
2019-03-20 10:51:34 -0700

    feat
    developed a new search method that pre-load all the data from database into a dictionary and directly access data from the dict. This search method increases query speed by up to 20 times.

--- Movie_App/movie_app/__init__.py
+++ Movie_App/movie_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, \
     url_for, flash, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
@@ -11,19 +12,34 @@ from operator import itemgetter
 from collections import OrderedDict
 import setting
 from spellchecker import SpellChecker
+import movie_data_formator
 
+import timeit
 
 app = Flask(__name__)
 
 APPLICATION_NAME = "Movie Search App"
+app.config['SQLALCHEMY_DATABASE_URI'] = setting.DB_URI
+app.config['SQLALCHEMY_POOL_SIZE'] = 100
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-engine = create_engine(setting.DB_URI)
+db = SQLAlchemy(app)
+session = db.session
+# engine = create_engine(setting.DB_URI, pool_size=100,pool_recycle=3600)
+#
+# Base.metadata.bind = engine
+#
+# DBSession = sessionmaker(bind=engine)
+# session = scoped_session(DBSession)()
 
-Base.metadata.bind = engine
+mi= session.query(IMDB_Movie_Info)
+mii = session.query(IMDB_Info_Index_Data)
+msi = session.query(IMDB_Summary_Index_Data)
 
-DBSession = sessionmaker(bind=engine)
-session = scoped_session(DBSession)()
-
+mi = movie_data_formator.movie_info_to_dict(mi)
+mii = movie_data_formator.movie_info_index_to_dict(mii)
+msi = movie_data_formator.movie_summary_index_to_dict(msi)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -132,6 +148,7 @@ def search_without_rating_count():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
+        start = timeit.default_timer()
         analyzer = Analyzer()
         search_query = request.form['query']
         stop_word = analyzer.stop_word(search_query)
@@ -153,7 +170,7 @@ def search():
         n = 0
         r = {}
         for movie_id, tf in result.items():
-            if len(r) < 1000:
+            if len(r) < 500:
                 # movie_info = session.query(IMDB_Movie_Info.title, IMDB_Movie_Info.year, IMDB_Movie_Info.serial).filter_by(id=movie_id).first()
                 movie_info = session.query(IMDB_Movie_Info).filter_by(id=movie_id).first()
 
@@ -188,23 +205,92 @@ def search():
 
         spell = SpellChecker()
         query = search_query.split(" ")
-        print(spell.correction('infinty'))
         correct = []
         for word in query:
             correct.append(spell.correction(word))
         correct = " ".join(correct)
         if correct == search_query:
             correct = ""
-        return render_template('result.html', result=r, correct=correct, query=search_query)
+        stop = timeit.default_timer()
+        return render_template('result.html', result=r, correct=correct, query=search_query, query_time=round(stop-start,2))
     else:
         return render_template('search.html')
 
-@app.route('/result', methods=['GET', 'POST'])
-def result(result, query):
+@app.route('/search_v2', methods=['GET', 'POST'])
+def search_v2():
+    start = timeit.default_timer()
     if request.method == 'POST':
-        pass
+        analyzer = Analyzer()
+        search_query = request.form['query']
+        stop_word = analyzer.stop_word(search_query)
+        stem = analyzer.stemming(stop_word)
+
+        info_result = Counter({})
+        summary_result = Counter({})
+        for word in set(stem.split(" ")):
+            if word in mii:
+                info_word = mii[word]
+                info_result += info_result + Counter(info_word['document_id'])
+
+            if word in msi:
+                summary_word = msi[word]
+                summary_result += summary_result + Counter(summary_word['document_id'])
+
+        result = info_result + summary_result
+        result = OrderedDict(sorted(result.items(), key=lambda d: d[1], reverse=True))
+
+        n = 0
+        r = {}
+        for movie_id, tf in result.items():
+            if len(r) < 500:
+                # movie_info = session.query(IMDB_Movie_Info.title, IMDB_Movie_Info.year, IMDB_Movie_Info.serial).filter_by(id=movie_id).first()
+                movie_info = session.query(IMDB_Movie_Info).filter_by(id=movie_id).first()
+                movie_info = mi[int(movie_id)]
+
+
+                if movie_info['run_time'] != "":
+                    run_time = movie_info['run_time'].split(" ")
+                    run_time = run_time[0].split(",")
+                    run_time = "".join(run_time)
+                    if float(run_time) > 80:
+
+                        r[n] = {}
+                        r[n]['title'] = movie_info['title']
+                        r[n]['year'] = movie_info['year']
+                        r[n]['certificate'] = movie_info['certificate']
+                        r[n]['run_time'] = movie_info['run_time']
+                        r[n]['genre'] = movie_info['genre']
+                        r[n]['rating'] = movie_info['rating']
+                        r[n]['rating_count'] = movie_info['rating_count']
+                        r[n]['gross'] = movie_info['gross']
+                        r[n]['actor'] = movie_info['actor']
+                        r[n]['serial'] = movie_info['serial']
+                        r[n]['tf'] = tf
+                        r[n]['rank'] = n
+                        n += 1
+            else:
+                break
+        rr = OrderedDict(sorted(r.items(), key=lambda d: d[1]['rating_count'], reverse=True))
+        for i, key in enumerate(rr):
+            r[key]['rank'] += i
+
+        r = OrderedDict(sorted(r.items(), key=lambda d: d[1]['rank'], reverse=False))
+
+        spell = SpellChecker()
+        query = search_query.split(" ")
+        correct = []
+        for word in query:
+            correct.append(spell.correction(word))
+        correct = " ".join(correct)
+        if correct == search_query:
+            correct = ""
+        stop = timeit.default_timer()
+        return render_template('result_v2.html', result=r, correct=correct, query=search_query, query_time=round(stop-start,2))
     else:
-        return redirect("/index")
+        return render_template('search_v2.html')
+
+
+
 if __name__ == '__main__':
     app.secret_key = "secret_key"
     app.debug = True
